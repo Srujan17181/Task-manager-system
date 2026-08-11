85c2f7c06852d8520d3990c77c6bac7a5099f958
Srujan17181 <srujanatsruju1718@gmail.com>
2026-08-11 17:54:41 +0530

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pg from "pg";
 
 dotenv.config();
+
+
+const db=new pg.Client({
+    user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    database:process.env.DB_NAME,
+    password:process.env.DB_PASSWORD
+});
+
+
+db.connect();
+
 
 const app=express();
 const port=3000;
@@ -20,6 +34,18 @@ app.use(cors({
 // middleware to handle body req
 
 app.use(express.json());
+
+db.query("SELECT NOW()",(err,res)=>{
+    if(err){
+        console.error("database is connected failure",err.message)
+    }
+    else{
+        console.log("database is connected");
+        console.log("server time",res.rows[0]);
+    }
+
+});
+
 
 app.listen(port,()=>{
     console.log(`app run in ${port}`)
